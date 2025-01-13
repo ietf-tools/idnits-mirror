116cfbfc47ca0e17bfc9d1cc71a043c279aea04f
Vladimir <vladimir.lutsenko@inmost.pro>
2025-01-13 15:29:02 +0200

    feat: validate document category(status)

--- tests/fixtures/base-doc.mjs
+++ tests/fixtures/base-doc.mjs
@@ -8,7 +8,8 @@ export const baseTXTDoc = {
       authors: [],
       date: null,
       source: null,
-      expires: null
+      expires: null,
+      intendedStatus: null
     },
     title: null,
     slug: null
