927fe51b12f1678fa201f383e6fd07078769ca1f
Dmutre <104783173+Dmutre@users.noreply.github.com>
2025-02-25 14:36:12 +0100

    fix: merge fix

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -446,6 +446,66 @@ describe('Parsing similar to RFC2119 boilerplate text', () => {
   })
 })
 
+describe('Parsing author address', () => {
+  test('Parsing author address', async () => {
+    const txt = `
+      ${metaTXTBlock}
+      ${tableOfContentsTXTBlock}
+      ${abstractWithReferencesTXTBlock}
+      ${introductionTXTBlock}
+      ${securityConsiderationsTXTBlock}
+      ${authorAddressTXTBlock}
+    `
+
+    const result = await parse(txt, 'txt')
+    expect(result.data.content.authorAddress).toEqual(expect.arrayContaining([expect.stringContaining('Authors\' Addresses')]))
+  })
+
+  test('Parsing author address with invalid character', async () => {
+    const txt = `
+      ${metaTXTBlock}
+      ${tableOfContentsTXTBlock}
+      ${abstractWithReferencesTXTBlock}
+      ${introductionTXTBlock}
+      ${securityConsiderationsTXTBlock}
+      Authors‘ Addresses
+    `
+
+    const result = await parse(txt, 'txt')
+    expect(result.data.content.authorAddress).toEqual(expect.arrayContaining([expect.stringContaining('Authors‘ Addresses')]))
+  })
+})
+
+describe('Parsing obsolete and update metadata', () => {
+  test('Parsing obsolete metadata', async () => {
+    const txt = `
+      ${metaTXTBlock}
+      ${tableOfContentsTXTBlock}
+      ${abstractWithReferencesTXTBlock}
+      ${introductionTXTBlock}
+      ${securityConsiderationsTXTBlock}
+    `
+
+    const result = await parse(txt, 'txt')
+    expect(result.data.extractedElements.obsoletesRfc).toEqual(['5678', '1234', '2345', '3456'])
+    expect(result.data.extractedElements.updatesRfc).toEqual(['6789', '7890', '8901', '9012'])
+  })
+
+  test('Parsing tetxt without obsolete and update metadata', async () => {
+    const txt = `
+      ${metaWithoutObsoleteAndUpdatesTXTBlock}
+      ${tableOfContentsTXTBlock}
+      ${abstractWithReferencesTXTBlock}
+      ${introductionTXTBlock}
+      ${securityConsiderationsTXTBlock}
+    `
+
+    const result = await parse(txt, 'txt')
+    expect(result.data.extractedElements.obsoletesRfc).toHaveLength(0)
+    expect(result.data.extractedElements.updatesRfc).toHaveLength(0)
+  })
+})
+
 describe('Parsing Category and Intended Status from document header', () => {
   test('Parses Category correctly', async () => {
     const txt = `
